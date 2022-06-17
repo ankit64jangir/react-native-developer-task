868ea830835a9fe80dd1d031c50e86a8d125d0b3
ankit64jangir <ankit2000jangir@gmail.com>
2022-06-17 21:27:28 +0530

    Done with Authentication screen

--- src/Screens/AuthenticationScreen.js
+++ src/Screens/AuthenticationScreen.js
@@ -1,14 +1,59 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Image,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import InputField from "../Components/InputField";
 import CustomButton from "../Components/CustomButton";
 
 const AuthenticationScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>AuthenticationScreen</Text>
-      <InputField label="Hello" />
-      <CustomButton btnTitle="Login Now" variant="secondary" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <View style={{ marginBottom: 20 }}>
+          <Image
+            source={require("../../assets/Logo.png")}
+            style={{ width: 40, height: 40 }}
+          />
+        </View>
+        <View style={styles.backgroundBox}>
+          <View style={styles.heading}>
+            <Text style={styles.heading5}>WELCOME BACK</Text>
+            <Text style={styles.heading1}>Log into your account</Text>
+          </View>
+          <View style={styles.inputContainer}>
+            <InputField
+              label="Email or Username"
+              placeholder="Enter your email"
+              type="text"
+            />
+            <View style={{ marginTop: 16 }} />
+            <InputField
+              label="Password"
+              placeholder="Enter your password"
+              type="password"
+              rightText="Forgot password?"
+            />
+            <View style={{ marginTop: 20 }} />
+            <CustomButton btnTitle="Login now" variant="primary" />
+          </View>
+
+          <Pressable style={styles.bottom}>
+            <Text style={{ ...styles.heading5, color: "#7F8084" }}>
+              Not registered yet?{" "}
+            </Text>
+            <Text style={{ ...styles.heading5, color: "#C5C7CA" }}>
+              Register <AntDesign name="arrowright" />
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -19,6 +64,36 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     paddingHorizontal: 20,
+  },
+  backgroundBox: {
+    backgroundColor: "#27292D",
+    borderWidth: 2,
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 30,
+    margin: 10,
+    borderColor: "#969696",
+    width: "100%",
+  },
+  inputContainer: {
+    marginTop: 34,
+  },
+  heading: {
+    alignItems: "center",
+  },
+  heading5: {
+    color: "#6B6C70",
+    fontSize: 14,
+  },
+  heading1: {
+    color: "#FFFFFF",
+    marginTop: 8,
+    fontSize: 18,
+  },
+  bottom: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 14,
   },
 });
 
